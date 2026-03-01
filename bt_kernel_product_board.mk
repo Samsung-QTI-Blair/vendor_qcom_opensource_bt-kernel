5fccb3531bc718200d5e0a0a5b0f4c3e0c8e8591
clexan <m.crnicki@outlook.com>
2026-03-01 22:39:12 +0100

    bt-kernel: Fix compilation error on Blair SoC

--- bt_kernel_product_board.mk
+++ bt_kernel_product_board.mk
@@ -1,9 +1,10 @@
 # Build BT kernel drivers
 PRODUCT_PACKAGES += btpower.ko\
 	bt_fm_slim.ko
-ifeq (,$(filter niobe anorak61 neo61 pitti, $(TARGET_BOARD_PLATFORM)))
-PRODUCT_PACKAGES += radio-i2c-rtc6226-qca.ko \
-	btfmcodec.ko \
+ifeq (,$(filter niobe anorak61 neo61 pitti taro blair kalama, $(TARGET_BOARD_PLATFORM)))
+PRODUCT_PACKAGES += btfmcodec.ko \
         bt_fm_swr.ko
 endif
-
+ifeq (,$(filter niobe anorak61 neo61 pitti seraph blair, $(TARGET_BOARD_PLATFORM)))
+PRODUCT_PACKAGES += radio-i2c-rtc6226-qca.ko
+endif
